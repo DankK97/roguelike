0207ce47a7fc40579dc1dc4459c593ff53a0ebe0
DankK97 <kecmand97@yahoo.com>
2015-05-23 12:50:50 -0400

    Create StrengthPotion.java

---
+++ StrengthPotion.java
@@ -0,0 +1,22 @@
+import asciiPanel.AsciiPanel;
+import java.awt.*;
+   
+public class StrengthPotion extends Item
+{
+   public StrengthPotion()
+   {
+      this.symbol = 'S';
+      this.name = "Strength Potion";
+      this.tooltip = "Increases your strengh!";
+      this.color = new Color(255, 128, 0);
+      this.x = 0;
+      this.y = 0;
+   }
+   
+   public void use(Player player)
+   {
+      player.strength += 1;
+      player.inventory.remove(this);
+   }
+}
+   
