19f6abd11170bc791c15e283f03edca57e28cee2
DankK97 <kecmand97@yahoo.com>
2015-05-23 12:49:58 -0400

    Update StartScreen.java

--- StartScreen.java
+++ StartScreen.java
@@ -1,21 +1,44 @@
 import java.awt.event.KeyEvent;
 import asciiPanel.AsciiPanel;
+import java.awt.*;
 
 public class StartScreen implements Screen {
-   public MenuItemList menuItems = new MenuItemList("Play", "Info", "Highscores");
-
-   @Override 
-   public void displayOutput(AsciiPanel terminal) {
-      terminal.writeCenter(" _________________________________________ ", 21, AsciiPanel.brightWhite);
-      terminal.writeCenter("/                                         \\", 22, AsciiPanel.brightWhite);
-      terminal.writeCenter("|        next generation roguelike        |", 23, AsciiPanel.brightWhite);
-      terminal.writeCenter("\\_________________________________________/", 24, AsciiPanel.brightWhite);
-      
-      menuItems.drawList(terminal, 40);
+   public MenuItemList menuItems = new MenuItemList("Singleplayer", "Multiplayer", "Scores", "Info", "Exit");
+   private String[] title;
+   private Color titleColor;
+   private World world;
+   
+   // Think of this as if its setup() in Processing
+   public StartScreen()
+   {
+         titleColor = new Color((int)(Math.random() * 100 + 155), (int)(Math.random() * 255), (int)(Math.random() * 100));
+         title = ArtReader.get("title");
+         world = new World(100, 50);
+         world.tiles = world.smooth(8);
    }
 
+   // Think of this as if its draw() in Processing
+   @Override 
+   public void displayOutput(AsciiPanel terminal)
+   {
+      drawWorldTiles(terminal);
+      ConsoleHelper.rectDouble(terminal, 3, 3, 94, 20);
+      ConsoleHelper.writeCenterArray(terminal, title, 5, titleColor);
+      menuItems.drawList(terminal, 15);
+   }
+
+   // This is the KeyListener
    @Override
    public Screen respondToUserInput(KeyEvent key) {
+      /* Code for choosing menu items. */
+      
+      /* ! HELP SCREENS ! */
+      if (key.getKeyCode() == KeyEvent.VK_S)
+         return new LetterScreen();
+      if (key.getKeyCode() == KeyEvent.VK_R)
+         return new RectangleScreen();
+      /* end HELP SCREENS */
+      
       switch (key.getKeyCode()){
          case KeyEvent.VK_UP: 
             this.menuItems.select(-1);
@@ -26,14 +49,30 @@ public class StartScreen implements Screen {
          case KeyEvent.VK_ENTER:
             switch(menuItems.selected){
                case 0:
-                  return new PlayScreen();
+                  return new NameScreen();
                case 1:
-                  return new InfoScreen();
+                  return new LoseScreen(world.player.name, 0);
                case 2:
-                  return new LoseScreen(); //change this to ScoreScreen
+                  return new ScoreScreen();
+               case 3:
+                  return new InfoScreen();
+               case 4:
+                  System.exit(0);
             }
       }
+      /* /menu code */
    	
       return this;
+   }
+   
+   public void drawWorldTiles(AsciiPanel terminal)
+   {
+      for (int o = 0; o < 100; o++)
+      {
+         for (int p = 0; p < 50; p++)
+         {
+            terminal.write(world.tile(o, p).symbol, o, p, world.tile(o, p).color);
+         }
+      }
    }
 }
