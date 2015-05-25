29f4c3d09d85f7a8ac90b1dd793055228202d3dc
DankK97 <kecmand97@yahoo.com>
2015-05-25 11:48:09 -0400

    Create Troll.java

---
+++ Troll.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import asciiPanel.AsciiPanel;
+
+public class Troll extends Monster
+{
+   Troll(World world)
+   {
+      super(world);
+      this.symbol = 't';
+      this.color = new Color(0, 128, 255);
+      this.health = 10;
+      this.strength = 1;
+      this.name = "Troll";
+   }
+   
+   Troll(World world, int x, int y)
+   {
+      super(world, x, y, 10, 1, 't', new Color(0, 128, 255));
+      this.name = "Troll";
+   }
+   
+   public void update()
+   {
+      int x = Math.abs(world.player.x - this.x);
+      if (x >= 1)
+         x /= Math.abs(world.player.x - this.x);
+      int y = Math.abs(world.player.y - this.y);
+      if (y >= 1)
+         y /= Math.abs(world.player.y - this.y);
+      this.move(2*x, 0);
+      this.move(0, 2*y);
+   }
+}
